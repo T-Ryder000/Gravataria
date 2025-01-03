8a8c83d2acae62c398e56e20434674d8d75d3ee6
Thyago <thiagogg020676@gmail.com>
2025-01-03 13:44:42 -0300

    ...

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: [
+    "./src/**/*.{html,js}", // Outros arquivos do diretório src, se houver
+    "./views/**/*.ejs"      // Adicione os arquivos EJS da pasta views
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
-
+};
