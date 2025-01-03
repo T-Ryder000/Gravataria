627566837c8988f0bca86be46e05cf5226319fb9
Thyago <thiagogg020676@gmail.com>
2025-01-03 13:35:46 -0300

    adjustings de directory

--- database/database.js
+++ database/database.js
@@ -1,8 +1,8 @@
-const Sequelize = require('sequelize')
+// const Sequelize = require('sequelize')
 
-const connection = new Sequelize('gravatariaking','root','98654321Thiago', {
-  host: 'localhost',
-  dialect:'mysql'
-})
+// const connection = new Sequelize('gravatariaking','root','98654321Thiago', {
+//   host: 'localhost',
+//   dialect:'mysql'
+// })
 
-module.exports = connection;
+// module.exports = connection;
